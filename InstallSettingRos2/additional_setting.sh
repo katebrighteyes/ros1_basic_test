d0d7531e9b553dd60aa16df25fcd8245579e27ca
Kate <49902595+katebrighteyes@users.noreply.github.com>
2025-04-14 21:34:03 +0900

    Add files via upload

---
+++ InstallSettingRos2/additional_setting.sh
@@ -0,0 +1,10 @@
+sudo apt install -y gazebo11
+sudo apt install -y ros-foxy-gazebo-ros-pkgs
+sudo apt install -y ros-foxy-cv-bridge ros-foxy-turtlesim
+
+sudo apt install -y ros-foxy-cartographer-ros ros-foxy-cartographer-ros-msgs
+
+sudo apt install -y ros-foxy-navigation2 ros-foxy-nav2-bringup 
+sudo apt install -y ros-foxy-xacro ros-foxy-joint-state-publisher-gui
+
+sudo apt install -y ros-foxy-nav2-map-server ros-foxy-nav2-planner ros-foxy-rqt-tf-tree
---
+++ InstallSettingRos2/setting.sh
@@ -0,0 +1,42 @@
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt install curl -y
+
+sudo apt update && sudo apt install -y \
+  libbullet-dev \
+  python3-pip \
+  python3-pytest-cov \
+  ros-dev-tools
+
+# install some pip packages needed for testing
+python3 -m pip install -U \
+  argcomplete \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest
+# install Fast-RTPS dependencies
+sudo apt install --no-install-recommends -y \
+  libasio-dev \
+  libtinyxml2-dev
+# install Cyclone DDS dependencies
+sudo apt install --no-install-recommends -y \
+  libcunit1-dev
+
+sudo apt-get install -y      vim   build-essential         cmake         git 
+sudo apt install -y python3-dev         python3-numpy         python3-matplotlib
+
+
+
+
+
+
